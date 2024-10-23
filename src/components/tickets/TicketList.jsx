e722fb7cd06f9d83b00ecb5d8baa2d74467dfa95
Jayden Palmer <jayalexpalmer@gmail.com>
2024-10-23 13:24:26 -0500

    ima be so real i dont remember the last time i pushed this

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -1,47 +1,53 @@
-import { useEffect, useState } from "react"
-import { getTickets } from "../../services/ticketService"
-import "./Tickets.css"
-import { Ticket } from "./Ticket"
-import { TicketFilterBar } from "./TicketFilterBar"
+import { useEffect, useState } from "react";
+import { getTickets } from "../../services/ticketService";
+import "./Tickets.css";
+import { Ticket } from "./Ticket";
+import { TicketFilterBar } from "./TicketFilterBar";
 
 export const TicketList = () => {
-const [allTickets, setAllTickets] = useState([])
-const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
-const [filteredTickets, setFilteredTickets] = useState([])
-const [searchTerm, setSearchTerm] = useState("")
+  const [allTickets, setAllTickets] = useState([]);
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getTickets().then((ticketsArray) => {
-    setAllTickets(ticketsArray)
-    console.log("tickets set!")
-  })
-  }, []) //ONLY runs on initial render of component BECAUSE its empty
+      setAllTickets(ticketsArray);
+      console.log("tickets set!");
+    });
+  }, []); //ONLY runs on initial render of component BECAUSE its empty
 
   useEffect(() => {
     if (showEmergencyOnly === true) {
-      const emergencyTickets = allTickets.filter(ticket => ticket.emergency === true)
-      setFilteredTickets(emergencyTickets)
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
     } else {
-      setFilteredTickets(allTickets)
+      setFilteredTickets(allTickets);
     }
-  }, [showEmergencyOnly, allTickets]) //not showEmergencyOnly. allTickets because the state is depending on allTickets to render intially
+  }, [showEmergencyOnly, allTickets]); //not showEmergencyOnly. allTickets because the state is depending on allTickets to render intially
 
   useEffect(() => {
-    const foundTickets = allTickets.filter((ticket) => 
-      ticket.description.toLowerCase().includes(searchTerm.toLowerCase()))
+    const foundTickets = allTickets.filter((ticket) =>
+      ticket.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-    setFilteredTickets(foundTickets)
-  }, [searchTerm, allTickets])
+    setFilteredTickets(foundTickets);
+  }, [searchTerm, allTickets]);
 
   return (
     <div className="tickets-container">
       <h2>TIckets</h2>
-      <TicketFilterBar setShowEmergencyOnly={setShowEmergencyOnly} setSearchTerm={setSearchTerm}/>
+      <TicketFilterBar
+        setShowEmergencyOnly={setShowEmergencyOnly}
+        setSearchTerm={setSearchTerm}
+      />
       <article className="tickets">
         {filteredTickets.map((ticketObj) => {
-          return <Ticket ticket={ticketObj} key={ticketObj.id}/>
+          return <Ticket ticket={ticketObj} key={ticketObj.id} />;
         })}
       </article>
     </div>
-  )
-}
+  );
+};
