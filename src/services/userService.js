24db4c28c7b31cadead190818f4178342c8db073
Jayden Palmer <jayalexpalmer@gmail.com>
2024-10-25 15:29:24 -0500

    added auth and login

--- src/services/userService.js
+++ src/services/userService.js
@@ -9,11 +9,12 @@ export const getStaff = () => {
     res.json()
   );
 };
+
 export const getUserByEmail = (email) => {
   return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
     res.json()
-  )
-}
+  );
+};
 
 export const createUser = (customer) => {
   return fetch("http://localhost:8088/users", {
@@ -22,5 +23,5 @@ export const createUser = (customer) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(customer),
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
